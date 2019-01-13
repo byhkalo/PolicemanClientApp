bd608c99860b2042bb47dfe40009f7c6c1544d7f
Konstantyn Bykhkalo <byhkalo.konstantyn@gmail.com>
2019-01-13 20:51:41 +0100

    Initial Commit

---
+++ PolicemanClientApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PolicemanClientApp
+//
+//  Created by Konstantyn on 1/13/19.
+//  Copyright © 2019 Konstantyn Bykhkalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
